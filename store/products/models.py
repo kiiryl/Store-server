461a40caf83c90c758d25aaecb3b4f900a77faf8
kiiryl <kiirillpoleshchuk@gmail.com>
2026-09-11 02:06:15 +0300

    10.02 before creating the basket

--- store/products/models.py
+++ store/products/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from users.models import User
+
 # Create your models here.
 
 class ProductCategory(models.Model):
@@ -19,3 +21,12 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Basket(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Корзина для {self.user.email} | Продукт: {self.product.name}"
